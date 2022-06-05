5d1780ea1a4914192f6989d970df305d168a5796
Luis Garcia <lalejandro.1988@gmail.com>
2022-06-04 19:06:21 -0600

    Principios de Middlewares- aplicado a ruta especifica

---
+++ pages/api/entries/[id]/_middleware.ts
@@ -0,0 +1,21 @@
+
+import mongoose from 'mongoose';
+import { NextRequest, NextFetchEvent } from 'next/server';
+
+export function middleware(req: NextRequest, ev: NextFetchEvent){
+    
+    const id = req.page.params?.id || '';
+
+    const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+
+    if( !checkMongoIDRegExp.test(id) ) {
+
+        return new Response( JSON.stringify({ message: 'El id no es válido ' + id }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+}
+
